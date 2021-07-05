213da05e6d03f1fcef14af151c6e8d5f32a14326
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-07-05 14:09:57 +0900

    エラーでってます。

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -10,6 +10,10 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
+  int numberOfRemaining;
+  int numberOfCorrect;
+  int correctRate;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -33,11 +37,45 @@ class _TestScreenState extends State<TestScreen> {
   }
 
   //TODO スコア表示部分
- Widget _scorePartU() {}
+ Widget _scorePartU() {
+    return Padding(padding: const EdgeInsets.only(left: 8.0, right: 8.0,top: 8.0 ),
+     child: Table(
+      children: [
+        TableRow(
+          children: [
+            Center(child: Text("のこり問題数",style: TextStyle(fontSize: 10.0),)),
+            Center(child: Text("正解数",style: TextStyle(fontSize: 10.0),)),
+            Center(child: Text("正答率",style: TextStyle(fontSize: 10.0),)),
+          ]
+        ),
+        TableRow(
+          children: [
+            Center(child: Text(numberOfRemaining.toString(),style: TextStyle(fontSize: 18.0),)),
+            Center(child: Text(numberOfCorrect.toString(),style: TextStyle(fontSize: 18.0),)),
+            Center(child: Text(correctRate.toString(),style: TextStyle(fontSize: 18.0),)),
+          ],
+        ),
+      ],
+    );// TODO ここではあとで書き換える
+ }
 //TODO 問題表示部分
- Widget _questionPart() {}
+ Widget _questionPart() {
+   return Container();// TODO ここではあとで書き換える
+ }
 
  //TODO 電卓ボタン部分
- Widget _calcButtons() {}
+ Widget _calcButtons() {
+   return Container();// TODO ここではあとで書き換える
+ }
+
+//TODO 答え合わせボタン
+  Widget _answerCheckButton() {
+    return Container();// TODO ここではあとで書き換える
+  }
+
+  //TODO 戻る
+  Widget _backButton() {
+    return Container();// TODO ここではあとで書き換える
+  }
 }
 
